2b0bdea267d7e2f00f80d07dd53a9097605562e8
QuanBao-dev <quy123bao20@gmail.com>
2022-07-25 12:45:57 -0700

    Done view cart page

---
+++ src/pages/Cart/Cart.jsx
@@ -0,0 +1,71 @@
+import { connect } from "react-redux";
+import { Link } from "react-router-dom";
+import Banner from "../../components/Banner/Banner";
+import "./Cart.css";
+
+const Cart = ({ cart }) => {
+  window.scroll({ top: 0 });
+  return (
+    <div>
+      <Banner url={window.location.href}></Banner>
+      <div className="cart-container">
+        <table className="cart-wrapper">
+          <thead>
+            <tr>
+              <th className="round-border-top-left"></th>
+              <th>Title</th>
+              <th>Price</th>
+              <th>Location</th>
+              <th>Rate</th>
+              <th className="round-border-top-right">Amount</th>
+            </tr>
+          </thead>
+          <tbody>
+            {cart.map(({ item, amount }, index) => (
+              <tr key={item.productId}>
+                <td
+                  className={
+                    index === cart.length - 1 ? "round-border-bottom-left" : ""
+                  }
+                >
+                  <img src={item.imageUrl} width="100px" alt="" />
+                </td>
+                <td>{item.title}</td>
+                <td>{item.price}</td>
+                <td>{item.location}</td>
+                <td>{item.rate}</td>
+                <td
+                  className={
+                    index === cart.length - 1 ? "round-border-bottom-right" : ""
+                  }
+                >
+                  {amount}
+                </td>
+              </tr>
+            ))}
+          </tbody>
+        </table>
+      </div>
+      <Link
+        to={"/shop/checkout"}
+        style={{
+          textDecoration: "none",
+        }}
+      >
+        <button className="cart-button-submit">Go to checkout</button>
+      </Link>
+    </div>
+  );
+};
+
+const mapStateToProps = (state) => {
+  return {
+    cart: state.shopCart.cart,
+  };
+};
+
+const mapDispatchToProps = () => {
+  return {};
+};
+
+export default connect(mapStateToProps, mapDispatchToProps)(Cart);
